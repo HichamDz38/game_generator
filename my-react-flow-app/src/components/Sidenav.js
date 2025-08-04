bf0dfb40ebe44b96e9c7724135b05accb83c76f8
aminedachir <dachiramine110@gmail.com>
2025-08-04 15:30:46 +0100

    liste of SCENARIOS

--- my-react-flow-app/src/components/Sidenav.js
+++ my-react-flow-app/src/components/Sidenav.js
@@ -1,11 +1,38 @@
 import React from 'react';
 //import { Background } from 'reactflow';
 import styles from './MyComponent.module.css';
+
+//flex: 0 0 49%; gap: 5px;
 function Sidenav() {
   return (
     <>
     <div className={styles.sidenav}>
-        <button>click me</button>
+        <h1 className={styles.title}>DASHBOARD SCENARIO</h1>
+        <button className={styles.button}>CREATE NEW SCENARIO</button>
+        <h2 className={styles.secondtitle}>LISTE OF SCENARIOS :</h2>
+        <div className={styles.scenariosbox}>
+          <p className={styles.name_scenario}>DEVICE ONE</p>
+          <div className={styles.buttonGroup}>
+            <button>EDIT</button>
+            <button>DELETE</button>
+          </div>
+        </div>
+        <br></br>
+        <div className={styles.scenariosbox}>
+          <p className={styles.name_scenario}>DEVICE ONE</p>
+          <div className={styles.buttonGroup}>
+            <button>EDIT</button>
+            <button>DELETE</button>
+          </div>
+        </div>
+        <br></br>
+        <div className={styles.scenariosbox}>
+          <p className={styles.name_scenario}>DEVICE ONE</p>
+          <div className={styles.buttonGroup}>
+            <button>EDIT</button>
+            <button>DELETE</button>
+          </div>
+        </div>
     </div>
     </>
   );
