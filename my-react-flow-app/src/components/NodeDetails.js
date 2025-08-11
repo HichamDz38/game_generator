ab67560402f1bdcdc3f3b2edd383f137c778ec1c
aminedachir <dachiramine110@gmail.com>
2025-08-11 20:52:04 +0100

    modify on configiration

--- my-react-flow-app/src/components/NodeDetails.js
+++ my-react-flow-app/src/components/NodeDetails.js
@@ -11,12 +11,12 @@ function NodeDetails({ nodeData, onClose }) {
         <button onClick={onClose} className={styles.closeButton}>×</button>
       </div>
       <div className={styles.nodeDetailsContent}>
-        <p><strong>ID:</strong> {nodeData.id}</p>
+        <p><strong>Name:</strong> {nodeData.data?.label}</p>
         <p><strong>Type:</strong> {nodeData.type}</p>
-        <p><strong>Label:</strong> {nodeData.data?.label}</p>
-        {nodeData.data?.deviceType && (
-          <p><strong>Device Type:</strong> {nodeData.data.deviceType}</p>
-        )}
+{/*         
+        {nodeData.data?.devicesdata && (
+          <p><strong>Device Type:</strong> {nodeData.data.devicesdata.name}</p>
+        )} */}
         <p><strong>Position:</strong> X: {nodeData.position?.x}, Y: {nodeData.position?.y}</p>
       </div>
     </div>
