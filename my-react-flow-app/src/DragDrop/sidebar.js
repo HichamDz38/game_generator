b2f6691bf063ddada671e6eeec38765009047f70
aminedachir <dachiramine110@gmail.com>
2025-08-11 14:23:41 +0100

    add static nodes input and output

--- my-react-flow-app/src/DragDrop/sidebar.js
+++ my-react-flow-app/src/DragDrop/sidebar.js
@@ -69,7 +69,14 @@ function Sidebar({ onLoadScenario }) {
           <div className="no-devices">No devices connected</div>
           
         )}
-        
+        <br></br><br></br>
+        <p className={styles.titledev2}>Static nodes</p>
+        <div className="dndnode device" onDragStart={(event) => onDragStart(event, 'input', 'Input Node')} draggable>
+          Input Node
+        </div>
+        <div className="dndnode device" onDragStart={(event) => onDragStart(event, 'output', 'Output Node')} draggable>
+          Output Node
+        </div>
 
       </div>
 
