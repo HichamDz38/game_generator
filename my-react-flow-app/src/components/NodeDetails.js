a64245137d2c768e0b4402ff8672b122a408fcc7
aminedachir <dachiramine110@gmail.com>
2025-08-12 17:28:52 +0100

    options problem

--- my-react-flow-app/src/components/NodeDetails.js
+++ my-react-flow-app/src/components/NodeDetails.js
@@ -8,28 +8,42 @@ function NodeDetails({ nodeData, onClose , OnDrop}) {
 
   return (
     <div className={styles.nodeDetailsContainer}>
-      <div className={styles.nodeDetailsHeader}>
-        <h4>Node informations</h4>
-        <button onClick={onClose} className={styles.closeButton}>×</button>
-      </div>
-      <div className={styles.nodeDetailsContent}>
-        <p><strong>Name:</strong> {nodeData.data?.label}</p>
-        <p><strong>Type:</strong> {nodeData.type}</p>
-        <div>
-          <div className={styles.nodeDetailsHeader2}>
-            <h4>Node configiration</h4>
+        <div className={styles.nodeDetailsHeader}>
+            <h4>Node informations</h4>
+            <button onClick={onClose} className={styles.closeButton}>×</button>
         </div>
-          {nodeData.data?.config?.map((item, index) => (
-            <div key={index} >
-              <p><strong>Name {i++} : </strong>{item.name}, Type: {item.type}</p>
-              {item.options && (
-                <p><strong>Options: </strong>{item.options.join(', ')}</p>
-              )}
+        <div className={styles.nodeDetailsContent}>
+            <p><strong>Name:</strong> {nodeData.data?.label}</p>
+            <p><strong>Type:</strong> {nodeData.type}</p>
+            <div>
+                <div className={styles.nodeDetailsHeader2}>
+                    <h4>Node configiration</h4>
+                </div>
+                {nodeData.data?.config?.map((item, index) => (
+                    <div key={index} >
+                    {/* <p><strong>Name {i++} : </strong>{item.name}, Type: {item.type}</p>
+                    {item.options && (
+                    <p><strong>Options: </strong>{item.options.join(', ')}</p>
+                    )} */}
+                        <label>
+                            {item.name}:
+                                {item.type === "select" ? (
+                                <select name={item.name}>
+                                    {item.option?.map((option, i) => (
+                                        <option key={i} value={option}>
+                                            {option}
+                                        </option>
+                                    ))}
+                                </select>
+                            ) : (
+                            <input name={item.name} type={item.type}/>
+                            )}
+                        </label>
+                    </div>
+                ))}
             </div>
-          ))}
-        </div>
         {/* <p><strong>Position:</strong> X: {nodeData.position?.x}, Y: {nodeData.position?.y}</p> */}
-      </div>
+        </div>
     </div>
   );
 }
